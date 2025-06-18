06eda6ea36538fac231de66c0e1b3117caccfaa5
moussaabbou <moussaabbou625@gmail.com>
2025-06-18 12:24:33 +0200

    modifer title and logo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import logo from 'components/photo/logo.png';
+import logo from './components/photo/logo.png';
 
 // تغيير عنوان الصفحة
 document.title = 'UsedShop';
@@ -11,7 +10,7 @@ document.title = 'UsedShop';
 // تغيير الأيقونة (favicon)
 const favicon = document.createElement('link');
 favicon.rel = 'icon';
-favicon.href = logo; // تأكد أن mylogo.ico موجود داخل مجلد public/
+favicon.href = logo; //
 document.head.appendChild(favicon);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
